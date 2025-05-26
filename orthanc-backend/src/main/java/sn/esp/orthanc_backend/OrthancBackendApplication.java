ac397237dd8c3a8ab5fdf2dfacda1cdd162909a9
MohamethLO123 <mohamethlo01@esp.sn>
2025-05-26 15:09:57 +0000

    Mis en place du backend

---
+++ orthanc-backend/src/main/java/sn/esp/orthanc_backend/OrthancBackendApplication.java
@@ -0,0 +1,13 @@
+package sn.esp.orthanc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrthancBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrthancBackendApplication.class, args);
+	}
+
+}
---
+++ orthanc-backend/src/test/java/sn/esp/orthanc_backend/OrthancBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.esp.orthanc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrthancBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
